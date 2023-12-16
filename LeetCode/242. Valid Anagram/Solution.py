3c7501d6b85e9aaead7be4ed99531e6a3cfba7da
Achraf Mansari <contact@achrafmansari.com>
2023-12-16 15:29:47 +0100

    Updated my solution for LeetCode problem 242 in Python

--- LeetCode/242. Valid Anagram/Solution.py
+++ LeetCode/242. Valid Anagram/Solution.py
@@ -1,7 +1,24 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) == len(t):
-            return sorted(s) == sorted(t)
 
-        else:
+        if len(s) != len(t):
             return False
+
+        letters_counter = {}
+
+        for s_char, t_char in zip(s, t):
+            if s_char in letters_counter:
+                letters_counter[s_char] += 1
+            else:
+                letters_counter[s_char] = 1
+
+            if t_char in letters_counter:
+                letters_counter[t_char] -= 1
+            else:
+                letters_counter[t_char] = -1
+
+        for key in letters_counter:
+            if letters_counter[key] != 0:
+                return False
+
+        return True
