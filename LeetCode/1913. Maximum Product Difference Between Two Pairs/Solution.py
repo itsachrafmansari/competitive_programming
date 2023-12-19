f93e3dc24a7790276c5415334e2828cd609cd9ed
Achraf Mansari <contact@achrafmansari.com>
2023-12-19 20:59:39 +0100

    Updated my solution of LeetCode problem 1913 in Python

--- LeetCode/1913. Maximum Product Difference Between Two Pairs/Solution.py
+++ LeetCode/1913. Maximum Product Difference Between Two Pairs/Solution.py
@@ -1,4 +1,21 @@
+from math import inf
+
+
 class Solution:
     def maxProductDifference(self, nums: list[int]) -> int:
-        nums.sort(reverse=True)
-        return (nums[0] * nums[1]) - (nums[-2] * nums[-1])
+        firstMax, secondMax, secondMin, firstMin = -inf, -inf, inf, inf
+
+        for num in nums:
+            if num > firstMax:
+                secondMax = firstMax
+                firstMax = num
+            elif num > secondMax:
+                secondMax = num
+            
+            if num < firstMin:
+                secondMin = firstMin
+                firstMin = num
+            elif num < secondMin:
+                secondMin = num
+
+        return (firstMax * secondMax) - (secondMin * firstMin)
